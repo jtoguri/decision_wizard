c80764c2843de863ba7913a37000ca278c8d38ba
Joshua Toguri <joshua.toguri@gmail.com>
2022-04-26 11:40:10 -0400

    add example seeds for the polls table

---
+++ db/seeds/02_polls.sql
@@ -0,0 +1,9 @@
+--Seeding the polls table
+INSERT INTO polls 
+  (question, admin_link, submission_link, creator_id, created_at, closed_at, vote_total) VALUES 
+    ('Where should we go to eat?', 'adminlink1.com', 'sublink1.com', 1,
+    '2022-01-08', '2022-01-13', 7);
+INSERT INTO polls 
+  (question, admin_link, submission_link, creator_id, created_at, closed_at, vote_total) VALUES 
+    ('What movie should we see?', 'adminlink2.com', 'sublink2.com', 2,
+    '2022-04-26', NULL, 3);
