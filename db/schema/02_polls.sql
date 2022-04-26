8e8c776c40851707b1266f134483ce2c3ec78bda
Joshua Toguri <joshua.toguri@gmail.com>
2022-04-26 11:56:15 -0400

    fix issues with polls table

--- db/schema/02_polls.sql
+++ db/schema/02_polls.sql
@@ -6,7 +6,7 @@ CREATE TABLE polls (
   question TEXT NOT NULL,
   admin_link TEXT NOT NULL,
   submission_link TEXT NOT NULL,
-  creator_id INT REFERENCES users(id) NOT NULL ON DELETE CASCADE,
+  creator_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   created_at DATE NOT NULL,
   closed_at DATE NULL,
   vote_total INT DEFAULT 0,
--- db/seeds/02_polls.sql
+++ db/seeds/02_polls.sql
@@ -4,6 +4,7 @@ INSERT INTO polls
     ('Where should we go to eat?', 'adminlink1.com', 'sublink1.com', 1,
     '2022-01-08', '2022-01-13', 7);
 INSERT INTO polls 
-  (question, admin_link, submission_link, creator_id, created_at, closed_at, vote_total) VALUES 
+  (question, admin_link, submission_link, creator_id, created_at,
+  closed_at, vote_total, max_votes) VALUES 
     ('What movie should we see?', 'adminlink2.com', 'sublink2.com', 2,
-    '2022-04-26', NULL, 3);
+    '2022-04-26', NULL, 3, 5);
