1d46886b31c6359c3eac0f75d662e11f87469162
Joshua Toguri <joshua.toguri@gmail.com>
2022-04-26 14:27:53 -0400

    remove the aggregate score column from the choices table

--- db/schema/03_choices.sql
+++ db/schema/03_choices.sql
@@ -3,8 +3,7 @@
 DROP TABLE IF EXISTS choices CASCADE;
 CREATE TABLE choices (
   id SERIAL PRIMARY KEY NOT NULL,
-  poll_id INT REFERENCES polls ON DELETE CASCADE,
+  poll_id INT NOT NULL REFERENCES polls ON DELETE CASCADE,
   title TEXT NOT NULL,
-  description TEXT NULL,
-  score INT DEFAULT 0
+  description TEXT NULL
 );
