f24b356665bf764969f6ef1c1ca0b7afc75deb1f
Joshua Toguri <joshua.toguri@gmail.com>
2022-04-26 12:12:18 -0400

    add seeds for the choices table

---
+++ db/seeds/03_choices.sql
@@ -0,0 +1,29 @@
+--Seeding the choices table
+INSERT INTO choices 
+  (poll_id, title, score) VALUES 
+    (1, 'Red Lobster', 3);
+INSERT INTO choices 
+  (poll_id, title, score) VALUES 
+    (1, 'McDonald''s', 4);
+INSERT INTO choices 
+  (poll_id, title, score) VALUES 
+    (1, 'Hot Dog Stand', 7);
+    
+INSERT INTO choices 
+  (poll_id, title) VALUES 
+    (2, 'Batman');
+INSERT INTO choices 
+  (poll_id, title) VALUES 
+    (2, 'Good Will Hunting');
+INSERT INTO choices 
+  (poll_id, title) VALUES 
+    (2, 'Little Women');
+INSERT INTO choices 
+  (poll_id, title) VALUES 
+    (2, 'Pulp Fiction');
+    
+    
+    
+    
+    
+    
