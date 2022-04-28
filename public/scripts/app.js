0b62c8b1937a590e312ff2ef86dc2c09ae0be074
Jeff Bush <jeffbushdesign@gmail.com>
2022-04-28 03:03:27 +0000

    Increment input name values when adding options

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,8 @@
 // Client facing scripts here
 $(document).ready(function () {
+  // Count all divs with .option class, use to increment input name with adding new options
+  const previousOption = document.querySelectorAll('.option').length;
+
   // Create poll form --> click button to add additional option fields
   $('#addOption').click(function (e) {
     // stops redirect
@@ -8,9 +11,9 @@ $(document).ready(function () {
       $(`
         <div class="option">
           <span>Option:</span>
-          <input type="text" name="" placeholder="" required><br>
+          <input type="text" name="titleOption${previousOption + 1}" placeholder="" required><br>
           <span>Description:</span>
-          <input type="text" name="" placeholder=""><br>
+          <input type="text" name="descriptionOption${previousOption + 1}" placeholder=""><br>
         </div>
         <br>;
       `);
