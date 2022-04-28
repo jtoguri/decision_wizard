ff88bb9e65f5cca965d104245051519cd24646ce
Jeff Bush <jeffbushdesign@gmail.com>
2022-04-28 13:36:51 -0400

    Add poll.ejs - starter elements of page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,31 @@
 // Client facing scripts here
 $(document).ready(function () {
-  
+
   // Create poll form --> click button to add additional option fields
-  $( '#addOption' ).click(function (e) {
+  $('#addOption').click(function (e) {
     // stops redirect
     e.preventDefault();
 
     // Count all divs with .option class, use to increment input name with adding new options
-    const previousOption = document.querySelectorAll('.option').length;
+    const numberOfOptions = document.querySelectorAll('.option').length;
 
     const newOption =
       $(`
         <div class="option">
           <span>Option:</span>
-          <input type="text" name="titleOption${previousOption + 1}" placeholder="" required><br>
+          <input type="text" name="titleOption${numberOfOptions + 1}" placeholder="" required><br>
           <span>Description:</span>
-          <input type="text" name="descriptionOption${previousOption + 1}" placeholder=""><br>
+          <input type="text" name="descriptionOption${numberOfOptions + 1}" placeholder=""><br>
         </div>
         <br>;
       `);
     $('#optionsContainer').append(newOption);
   });
-  
-  $( "#pollForm" ).submit(function( e ) {
+
+  $("#pollForm").submit(function (e) {
     e.preventDefault();
-    console.log($( this ).serialize());
-    $.post( "/api/polls", $( this ).serialize(), function( data ) {
+    console.log($(this).serialize());
+    $.post("/api/polls", $(this).serialize(), function (data) {
       console.log(data);
     });
   });
