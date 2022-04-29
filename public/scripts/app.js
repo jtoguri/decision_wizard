96ea66eb16a9a0c053da0efec33c163f567fd814
serra <serra@modemgirl.ca>
2022-04-28 19:39:59 -0700

    saving work in progress

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,27 +1,47 @@
 // Client facing scripts here
+
 $(document).ready(function () {
-  
+
   // Create poll form --> click button to add additional option fields
-  $( '#addOption' ).click(function (e) {
+  $( '.addOption' ).on("click", function (e) {
     // stops redirect
-    e.preventDefault();
+    e.preventDefault(e);
+
+    console.log("adding an option!");
 
     // Count all divs with .option class, use to increment input name with adding new options
     const previousOption = document.querySelectorAll('.option').length;
 
     const newOption =
-      $(`
-        <div class="option">
-          <span>Option:</span>
-          <input type="text" name="titleOption${previousOption + 1}" placeholder="" required><br>
-          <span>Description:</span>
-          <input type="text" name="descriptionOption${previousOption + 1}" placeholder=""><br>
-        </div>
-        <br>;
-      `);
+      // $(`
+      //   <div class="input-group option">
+      //     <input type="text" name="choices[choice${previousOption + 1}][title]" placeholder="New option" required>
+      //     <input class="form-control" type="text" name="choices[choice${previousOption + 1}][describe]" placeholder="new" /><br />
+      //     <button class="addOption btn btn-outline-secondary btn-sm" type="button">+</button>
+      //   </div>
+      // `);
+        $(`
+      <div class="input-group option">
+      <input class="form-control" type="text" name="choices[choice${previousOption + 1}][title]" placeholder="New option" required />
+      <input class="form-control" type="text" name="choices[choice${previousOption + 1}][describe]" placeholder="new decription" />
+      <button class="btn btn-outline-secondary btn-sm addOption" type="button">+</button>
+    </div>
+    `);
+
+
+      $(this).removeClass('addOption').addClass('removeOption');
+      //change inner html
     $('#optionsContainer').append(newOption);
+
   });
-  
+
+  $('.removeOption').on("click", function (e){
+    e.preventDefault(e);
+    $(this).closest('div').remove();
+
+  })
+
+
   $( "#pollForm" ).submit(function( e ) {
     e.preventDefault();
     console.log($( this ).serialize());
