eb8bf82e378ac6914bfc13798cf4e7571aad4252
serra <serra@modemgirl.ca>
2022-04-29 11:11:04 -0700

    remove + button from form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,53 +1,41 @@
 // Client facing scripts here
 
-$(document).ready(function () {
+
+
+
+
+$(document).ready(function() {
 
   // Create poll form --> click button to add additional option fields
-  $( '.addOption' ).on("click", function (e) {
+  $('.addOption').on("click", function(e) {
     // stops redirect
-    e.preventDefault(e);
-
-    console.log("adding an option!");
+    e.preventDefault();
 
     // Count all divs with .option class, use to increment input name with adding new options
     const numberOfOptions = document.querySelectorAll('.option').length;
 
-    const newOption =
-      // $(`
-      //   <div class="input-group option">
-      //     <input type="text" name="choices[choice${previousOption + 1}][title]" placeholder="New option" required>
-      //     <input class="form-control" type="text" name="choices[choice${previousOption + 1}][describe]" placeholder="new" /><br />
-      //     <button class="addOption btn btn-outline-secondary btn-sm" type="button">+</button>
-      //   </div>
-      // `);
-        $(`
-      <div class="input-group option">
-      <input class="form-control" type="text" name="choices[choice${previousOption + 1}][title]" placeholder="New option" required />
-      <input class="form-control" type="text" name="choices[choice${previousOption + 1}][describe]" placeholder="new decription" />
-      <button class="btn btn-outline-secondary btn-sm addOption" type="button">+</button>
-    </div>
-    `);
-
-
-      $(this).removeClass('addOption').addClass('removeOption');
-      //change inner html
+    const newOption = $(`
+    <div class="input-group option">
+    <input class="form-control" type="text" name="choices[choice${numberOfOptions + 1}][title]" placeholder="New option" required />
+    <input class="form-control" type="text" name="choices[choice${numberOfOptions + 1}][describe]" placeholder="new decription" />
+    <button class="btn btn-outline-secondary btn-sm removeOption" type="button"> - </button>
+    </div>`
+    );
     $('#optionsContainer').append(newOption);
-
   });
 
-  $('.removeOption').on("click", function (e){
-    e.preventDefault(e);
+  $('.removeOption').on("click", function(e) {
+    e.preventDefault();
     $(this).closest('div').remove();
+  });
 
-  })
 
-
-  $( "#pollForm" ).submit(function( e ) {
+  $("#pollForm").submit(function(e) {
     e.preventDefault();
 
-    const pollData = $( this ).serialize();
+    const pollData = $(this).serialize();
 
-    $.post( "/api/polls", pollData, function( newPoll ) {
+    $.post("/api/polls", pollData, function(newPoll) {
       const preview = $(`
         <div>
           <h3>${newPoll.question}</h3>
@@ -57,10 +45,10 @@ $(document).ready(function () {
       `);
 
       for (const choice of newPoll.choices) {
-        $( preview ).find( 'ul' ).append(`<li>${choice.title}</li>`);
+        $(preview).find('ul').append(`<li>${choice.title}</li>`);
       }
 
-      $( '#pollPreview' ).append(preview);
+      $('#pollPreview').append(preview);
     });
   });
 });
