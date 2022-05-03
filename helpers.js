40e69605371de7235bf63520b89245ccb5765a98
serra <serra@modemgirl.ca>
2022-05-03 11:10:48 -0700

    refactor helpers

--- helpers.js
+++ helpers.js
@@ -10,33 +10,29 @@ const mg = mailgun.client({ username: 'api', key: process.env.MG_PK });
 
 
 //on vote submission:
-//Send 'username' has voted for '#1 choice' in your 'what shoud we eat for dinner' poll
-
-const sendMail = (info) => {
-  const email = info.rows[0].email;
-  const question = info.rows[0].question;
-  const url = `http://localhost:8080/api/polls/${info.rows[0].id}`;
-
-  mg.messages.create('sandboxccb09bee7c9a4f58bdecfb63b9aa73ba.mailgun.org', {
-    from: `Decision Wizard <fjord@${domain}>`,
-    to: email,
-    subject: `${question} has a new response!`,
-    text: `${question} has a new response!`,
-    html: `
-    <h3>${question} has a new response!</h3><br>
-    Click <a href="${url}">here</a> to see the results.`
-  })
-    .then(msg => console.log(msg)) // logs response data
-    .catch(err => console.log(err)); // logs any error
-
-
-};
 
 module.exports = {
   generateExternalPollId: () => {
     return uuidv4();
   },
-  sendMail
+  sendMail: (info) => {
+    const email = info.rows[0].email;
+    const question = info.rows[0].question;
+    const url = `http://localhost:8080/api/polls/${info.rows[0].id}`;
+
+    mg.messages.create('sandboxccb09bee7c9a4f58bdecfb63b9aa73ba.mailgun.org', {
+      from: `Decision Wizard <fjord@${domain}>`,
+      to: email,
+      subject: `${question} has a new response!`,
+      text: `${question} has a new response!`,
+      html: `
+      <h3>${question} has a new response!</h3><br>
+      Click <a href="${url}">here</a> to see the results.`
+    })
+      .then(msg => console.log(msg)) // logs response data
+      .catch(err => console.log(err)); // logs any error
+  }
+
 };
 
 
