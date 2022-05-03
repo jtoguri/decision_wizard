27470bb171f310c1787449eaa0749dbcbc909fcc
Joshua Toguri <joshua.toguri@gmail.com>
2022-05-03 09:38:32 -0400

    modify paths for the links of the poll seeds

--- db/seeds/02_polls.sql
+++ db/seeds/02_polls.sql
@@ -3,11 +3,11 @@ INSERT INTO polls
   (external_uuid, question, admin_link, submission_link, creator_id, created_at,
   closed_at, choice_count) VALUES 
     ('randomString1', 'Where should we go to eat?',
-    '/api/polls/randomString1/admin', '/api/polls/randomString1', 1,
+    'api/polls/randomString1/admin', 'api/polls/randomString1', 1,
     '2022-01-08', '2022-01-13', 3);
 INSERT INTO polls 
   (external_uuid, question, admin_link, submission_link, creator_id, created_at,
   closed_at, choice_count) VALUES 
     ('randomString2', 'What movie should we see?',
-    '/api/polls/randomString2/admin', '/api/polls/randomString2', 2,
+    'api/polls/randomString2/admin', 'api/polls/randomString2', 2,
     '2022-04-26', NULL, 4);
