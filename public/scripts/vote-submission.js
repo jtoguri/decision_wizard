f8f4cf75a12c290b002aba978ffa7f66e7c73163
Jeff Bush <jeffbushdesign@gmail.com>
2022-05-06 13:19:05 -0400

    Add margin-bottom below results table on Admin page

--- public/scripts/vote-submission.js
+++ public/scripts/vote-submission.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
         <table id="resultsTable">
           <thead>
             <tr>
-              <th class="tableHeading" scope="col">Choices</th>
+              <th class="tableHeading" scope="col">CHOICES</th>
               <th class="tableHeading" scope="col">SCORE</th>
             </tr>
           </thead>
@@ -39,15 +39,15 @@ $(document).ready(function () {
       $('.container').empty().append(result);
     });
   });
-  
+
   $(".showDescription").on("click", function () {
-    if ($( this ).siblings(".description").css("display") === "none") {
-      $( this ).text("hide description");
-      $( this ).siblings(".description").slideDown();
+    if ($(this).siblings(".description").css("display") === "none") {
+      $(this).text("hide description");
+      $(this).siblings(".description").slideDown();
       return;
     }
 
-    $( this ).siblings(".description").slideUp();
-    $( this ).text("view description");
+    $(this).siblings(".description").slideUp();
+    $(this).text("view description");
   });
 });
