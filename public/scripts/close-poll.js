4624fdaa9657c846d310e450f1d0ef3375e0be58
serra <serra@modemgirl.ca>
2022-05-06 07:05:23 -0700

    add close poll functionality

---
+++ public/scripts/close-poll.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+  $("#closePoll").on("click", function() {
+    // navigator.clipboard.writeText(`http://${$("a.shareLink").text()}`);
+    console.log('im gonna close this poll!!!');
+  });
+});
