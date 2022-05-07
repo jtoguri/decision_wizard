f733a1af107ee6f8dd9902887bf94375333cd967
Joshua Toguri <joshua.toguri@gmail.com>
2022-05-07 13:00:21 -0400

    adding vote seeds for the cars table

--- db/seeds/04_votes.sql
+++ db/seeds/04_votes.sql
@@ -38,3 +38,75 @@ INSERT INTO votes
 INSERT INTO votes 
   (choice_id, position) VALUES 
     (3, 1);
+
+INSERT INTO votes
+  (choice_id, position) VALUES
+    (12, 1); 
+
+INSERT INTO votes
+  (choice_id, position) VALUES
+    (7, 2); 
+    
+INSERT INTO votes
+  (choice_id, position) VALUES
+    (8, 3); 
+
+INSERT INTO votes
+  (choice_id, position) VALUES
+    (9, 4); 
+
+INSERT INTO votes
+  (choice_id, position) VALUES
+    (10, 5); 
+
+INSERT INTO votes
+  (choice_id, position) VALUES
+    (11, 6);
+
+INSERT INTO votes
+  (choice_id, position) VALUES
+    (12, 1); 
+
+INSERT INTO votes
+  (choice_id, position) VALUES
+    (8, 2); 
+
+INSERT INTO votes
+  (choice_id, position) VALUES
+    (9, 3); 
+
+INSERT INTO votes
+  (choice_id, position) VALUES
+    (7, 4); 
+
+INSERT INTO votes
+  (choice_id, position) VALUES
+    (11, 5); 
+
+INSERT INTO votes
+  (choice_id, position) VALUES
+    (10, 6);
+
+INSERT INTO votes
+  (choice_id, position) VALUES
+    (12, 1); 
+
+INSERT INTO votes
+  (choice_id, position) VALUES
+    (10, 2); 
+
+INSERT INTO votes
+  (choice_id, position) VALUES
+    (9, 3); 
+
+INSERT INTO votes
+  (choice_id, position) VALUES
+    (7, 4); 
+
+INSERT INTO votes
+  (choice_id, position) VALUES
+    (8, 5); 
+
+INSERT INTO votes
+  (choice_id, position) VALUES
+    (11, 6);
